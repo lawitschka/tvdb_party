a58a360a91e54ad3b8038d239678c0bdf4642b39
Moritz Lawitschka <moritz@urbanvention.com>
2013-03-23 11:17:05 +0100

    Include Rating and Rating Count in Banners

--- lib/tvdb_party/banner.rb
+++ lib/tvdb_party/banner.rb
@@ -1,15 +1,17 @@
 module TvdbParty
   class Banner
-    attr_accessor :banner_type, :banner_type2, :season, :path, :thumbnail_path, :language
-    
+    attr_accessor :banner_type, :banner_type2, :season, :path, :thumbnail_path, :language, :rating, :rating_count
+
     def initialize(options={})
       @banner_type = options["BannerType"]
       @banner_type2 = options["BannerType2"]
       @season = options["Season"]
       @path = options["BannerPath"]
       @language = options["Language"]
+      @rating = options["Rating"].to_f
+      @rating_count = options["RatingCount"].to_i
     end
-    
+
     def url
       "http://thetvdb.com/banners/" + @path
     end
